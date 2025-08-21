9499bbadf47fe7a12635d4f5c2adc155ddbf9aef
Nandana-Neo <nandana715tvm@gmail.com>
2025-08-21 18:39:31 +0530

    [FIX]: restructuring fs

--- learn/ST1/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-yacc yacc.y -d
-lex lex.l
-gcc y.tab.c lex.yy.c exptree.c -o a.out
-./a.out
-
-# Run `./xsm -l library.lib -e learn/ST1/exp.xsm` in console in parent folder xsm_expl
